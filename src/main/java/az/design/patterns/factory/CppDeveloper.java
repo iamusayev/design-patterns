e142847013b59448801e7d248d9ef370a374a738
iamusayev <anarmusayev1337@gmail.com>
2023-02-16 16:47:45 +0400

    Added Factory Method pattern implementation

---
+++ src/main/java/az/design/patterns/factory/CppDeveloper.java
@@ -0,0 +1,8 @@
+package az.design.patterns.factory;
+
+public class CppDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Cpp developer writes Java code");
+    }
+}
---
+++ src/main/java/az/design/patterns/factory/PhpDeveloper.java
@@ -0,0 +1,8 @@
+package az.design.patterns.factory;
+
+public class PhpDeveloper implements Developer {
+    @Override
+    public void writeCode() {
+        System.out.println("Php developer writes Php code...");
+    }
+}
