fa9db215aa98ea3caa67a7eb38a6b41b739d6ee4
Anar <95879636+iamusayev@users.noreply.github.com>
2023-08-11 23:46:06 +0400

    Composite (#56)

---
+++ src/main/java/az/design/patterns/structural/bridge/CppDeveloper.java
@@ -0,0 +1,9 @@
+package az.design.patterns.structural.bridge;
+
+public class CppDeveloper implements Developer {
+
+    @Override
+    public void writeCode() {
+        System.out.println("C++ developer writes C++ code...");
+    }
+}
